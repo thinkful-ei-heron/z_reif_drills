c300ca55090a05bb530f026df32d7b070232744c
Bridger <bridgerrhammond@gmail.com>
2019-09-16 16:10:12 -0500

    eslint use strict

--- src/app.js
+++ src/app.js
@@ -1,26 +1,28 @@
+'use strict';
+
 function getYearOfBirth(age) {
   return 2019 - age;
 }
 
 function createGreeting(name, age) {
   if (name === undefined || age === undefined) {
-    throw new Error("Something is undefined");
+    throw new Error('Something is undefined');
   }
 
   if (age < 0) {
-    throw new Error("Age can not be negative.");
+    throw new Error('Age can not be negative.');
   }
 
-  if (typeof age !== "number") {
-    throw new Error("Age is not a number.");
+  if (typeof age !== 'number') {
+    throw new Error('Age is not a number.');
   }
   const yob = getYearOfBirth(age);
   return `Hi my name is ${name} and I'm ${age} years old and I was born in ${yob}.`;
 }
 
 try {
-  const greeting1 = createGreeting("Zee", 29);
+  const greeting1 = createGreeting('Rich', 18);
   console.log(greeting1);
-} catch (e) {
+} catch(e) {
   console.error(e.message);
 }
