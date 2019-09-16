a5ccd194ca109ceda34038aa2d43ded417749ccf
Zee Celest <zeecelest@gmail.com>
2019-09-16 15:57:29 -0400

    added function

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
-console.log('Hello my name is Zee and I\'m 42.');
+// console.log('Hello my name is Zee and I\'m 42.');
+
+function createGreeting () {
+    console.log('Hello my name is Zee and I\'m 42.');
+}
+createGreeting();
