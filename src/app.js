0c295acb623c6a2ec822397e03798830443aa44b
Zee Celest <zeecelest@gmail.com>
2019-09-16 16:13:08 -0400

    refactored year of birth

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 // console.log('Hello my name is Zee and I\'m 42.');
 
 function createGreeting (name,age) {
+    // const yearOfBirth = 2019 - age;
+    return (`Hello my name is ${name} and I'm ${age}. `);
+}
+
+function getYearOfBirth (age) {
     const yearOfBirth = 2019 - age;
-    return (`Hello my name is ${name} and I'm ${age}. I was born in ${yearOfBirth}`);
+    return (`I was born in ${yearOfBirth}.`);
 }
 const greeting1 = createGreeting('Zee', 42);
 console.log(greeting1);
