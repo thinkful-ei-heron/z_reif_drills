045fda9123a43d13b65b05a314b2ff20904f4308
Zee Celest <zeecelest@gmail.com>
2019-09-16 16:03:38 -0400

    changed greeting

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 // console.log('Hello my name is Zee and I\'m 42.');
 
-function createGreeting () {
-    return ('Hello my name is Zee and I\'m 42.');
+function createGreeting (name,age) {
+    return (`Hello my name is ${name} and I'm ${age}.`);
 }
 const greeting1 = createGreeting();
 console.log(greeting1);
