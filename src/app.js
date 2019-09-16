0aafd9ff89abc001e89f0018a81ab826bd899765
Zee Celest <zeecelest@gmail.com>
2019-09-16 16:09:31 -0400

    updated function

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 // console.log('Hello my name is Zee and I\'m 42.');
 
 function createGreeting (name,age) {
-    return (`Hello my name is ${name} and I'm ${age}.`);
+    const yearOfBirth = 2019 - age;
+    return (`Hello my name is ${name} and I'm ${age}. I was born in ${yearOfBirth}`);
 }
-const greeting1 = createGreeting();
+const greeting1 = createGreeting('Zee', 42);
 console.log(greeting1);
